4bc56addfdafcf21cfe63caaf6e948ad00b30a2c
Zajjith Vedha <zajjith@gmail.com>
2021-09-25 17:43:24 +0530

    single event banner image and contact details fixed

--- resources/views/frontend/single_event.blade.php
+++ resources/views/frontend/single_event.blade.php
@@ -17,7 +17,7 @@
 
                 <div class="row">
                     <div class="col-12">
-                        <img src="{{ url('images/events', $event->image) }}" alt="" class="img-fluid">
+                        <img src="{{ url('images/events', $event->image) }}" alt="" class="img-fluid w-100" style="height: 25rem; object-fit: cover;">
                     </div>
                 </div>
 
@@ -29,21 +29,54 @@
                 </div>
 
                 <div class="details mt-4">
-                    <h6 class="fw-bold mb-3" style="color: #800f0e;">Event Date & Time: <span class="fw-normal gray ms-3">{{ $event->date }} - {{ $event->time }}</span></h6>
 
-                    <h6 class="fw-bold mb-3" style="color: #800f0e;">Event Location: <span class="fw-normal gray ms-3">{{ $event->city }} - {{ $event->country }}</span></h6>
+                    <div class="row justify-content-center">
+                        <div class="col-8">
+                            <div class="row">
+                                <div class="col-6">
+                                    <h6 class="fw-bold mb-3" style="color: #800f0e;">Event date & time: </h6>
+                                </div>
 
-                    <h6 class="fw-bold mb-3" style="color: #800f0e;">Event Type: <span class="fw-normal gray ms-3">{{ $event->type }}</span></h6>
+                                <div class="col-6">
+                                    <h6 class="fw-normal gray ms-3">{{ $event->date }} - {{ $event->time }}</h6>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-6">
+                                    <h6 class="fw-bold mb-3" style="color: #800f0e;">Event location: </h6>
+                                </div>
+
+                                <div class="col-6">
+                                    <h6 class="fw-normal gray ms-3">{{ $event->city }} - {{ $event->country }}</h6>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-6">
+                                    <h6 class="fw-bold mb-3" style="color: #800f0e;">Event type:</h6>
+                                </div>
+
+                                <div class="col-6">
+                                    <h6 class="fw-normal gray ms-3">{{ $event->type }}</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
 
                 <div class="row mt-5">
                     <div class="col-6 text-end">
-                        <a href="{{ $event->url }}" class="text-decoration-none buttons p-2" type="button" target="_blank">Register to the event</a>
+                        @if($event->url != null)
+                            <a href="{{ $event->url }}" class="text-decoration-none buttons p-2" type="button" target="_blank">Register to the event</a>
+                        @else
+                            <button class="text-decoration-none buttons p-2" type="button" data-bs-toggle="modal" data-bs-target="#organizer">Contact Organizer</button>
+                        @endif
                     </div>
 
                     <div class="col-6 text-start">
-                        <a href="{{ route('frontend.events') }}" class="text-decoration-none buttons p-2" type="button">Add Your Event</a>
+                        <a href="{{ route('frontend.events') }}" class="text-decoration-none buttons p-2" type="button">Add your event</a>
                     </div>
                 </div>
 
@@ -66,6 +99,42 @@
                         </div>
                     </a>
                 @endforeach
+            </div>
+        </div>
+    </div>
+
+
+    <div class="modal" id="organizer" tabindex="-1" aria-labelledby="organizer" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Event organizer contact details</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <h6 class="fw-bold mb-3" style="color: #800f0e;">Event organizer email: </h6>
+                        </div>
+
+                        <div class="col-6">
+                            <h6 class="fw-normal gray ms-3">{{ $event->organizer_email }}</h6>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <h6 class="fw-bold mb-3" style="color: #800f0e;">Event organizer phone: </h6>
+                        </div>
+
+                        <div class="col-6">
+                            <h6 class="fw-normal gray ms-3">{{ $event->organizer_phone }}</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
             </div>
         </div>
     </div>

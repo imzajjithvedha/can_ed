9d6e5dfa7820d6c1156833134157809432ba61f5
Zajjith Vedha <zajjith@gmail.com>
2021-11-08 23:11:23 +0530

    banner search controller error fixed

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Models\Articles;
 use App\Models\Businesses;
 use App\Models\Videos;

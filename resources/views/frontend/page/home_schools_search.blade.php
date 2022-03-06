9ce28c792cb51bd512b769af45bf48485b2ca025
Zajjith Vedha <zajjith@gmail.com>
2022-03-06 17:09:51 +0530

    scholl search link updated

--- resources/views/frontend/page/home_schools_search.blade.php
+++ resources/views/frontend/page/home_schools_search.blade.php
@@ -26,16 +26,16 @@
                     <div class="row justify-content-between border mb-4 py-3 mx-0">
                         <div class="col-5 text-center">
                             @if($school->featured_image != null)
-                                <img src="{{ uploaded_asset($school->featured_image) }}" class="card-img-top img-fluid w-100" style="height: 15rem; object-fir: cover;" alt="...">
+                                <a href="{{ route('frontend.single_school', [$school->id, $school->slug]) }}" class="text-decoration-none"><img src="{{ uploaded_asset($school->featured_image) }}" class="card-img-top img-fluid w-100" style="height: 15rem; object-fir: cover;" alt="..."></a>
                             @else
-                                <img src="{{ url('img/frontend/no_image.jpg') }}" alt="" class="img-fluid w-100" style="height: 15rem; object-fit: cover;">
+                                <a href="{{ route('frontend.single_school', [$school->id, $school->slug]) }}" class="text-decoration-none"><img src="{{ url('img/frontend/no_image.jpg') }}" alt="" class="img-fluid w-100" style="height: 15rem; object-fit: cover;"></a>
                             @endif
 
                             <a href="{{ route('frontend.single_school', [$school->id, $school->slug, '#tab-contact']) }}" type="button" class="btn text-white advanced-btn mt-3 w-50">Contact us</a>
                         </div>
 
                         <div class="col-7">
-                            <h4 class="fw-bolder futura">{{ $school->name }}</h4>
+                            <a href="{{ route('frontend.single_school', [$school->id, $school->slug]) }}" class="text-decoration-none"><h4 class="fw-bolder futura">{{ $school->name }}</h4></a>
                             <div class="gray mt-2" style="text-align: justify; overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 9; -webkit-box-orient: vertical;">{!! $school->quick_facts_title_1_paragraph !!}</div>
 
                             <div class="row align-end mt-4 justify-content-end">
